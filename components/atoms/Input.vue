d9246521c5b98411a6254afcd35f1c36818d22c2
Tatsuya <mochiduki.t222@gmail.com>
2023-03-21 02:47:38 +0900

    add: input component

---
+++ components/atoms/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <input :type="type" :name="name" :id="id" :placeholder="placeholder" :value="value">
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  type?: string,
+  name?: string,
+  id?: string,
+  placeholder?: string,
+  value?: string | number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'text',
+  name: '',
+  id: '',
+  placeholder: '',
+  value: ''
+})
+</script>
