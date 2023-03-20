62fcc7db9114d59dff03806ba007d743d4cf12b2
Tatsuya <mochiduki.t222@gmail.com>
2023-03-21 02:47:30 +0900

    add: Button component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button type="button" class="w-full focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  text: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  text: ''
+})
+</script>
