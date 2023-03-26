14e49c2f5a72e833da8b92bc6f24285aa7ac458b
Tatsuya <mochiduki.t222@gmail.com>
2023-03-27 01:25:32 +0900

    update: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  runtimeConfig: {
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_ATUH_DOMAIN: process.env.FIREBASE_ATUH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+      FIREBASE_MEASUEEMENT_ID: process.env.FIREBASE_MEASUEEMENT_ID
+    }
+
+  },
   css: ['@/assets/css/tailwind.css'],
   postcss: {
     plugins: {
